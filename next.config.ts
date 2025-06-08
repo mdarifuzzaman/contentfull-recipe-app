f7c8bf536035e4ce40d2391de424a898905d5930
Arif Uzzaman <arif_uap@outlook.com>
2025-06-08 22:30:41 +1000

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: true,  
+  images: {
+    remotePatterns: [{ hostname: 'images.ctfassets.net'}]
+  }
 };
 
 export default nextConfig;
